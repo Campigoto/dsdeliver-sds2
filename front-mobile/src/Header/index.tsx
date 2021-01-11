e958142bfbc781819d93406f9f4b705eb58faf59
campigoto <campigoto@hotmail.com>
2021-01-10 21:00:13 -0300

    Fix total

---
+++ front-mobile/src/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Text>DS Delivery</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#DA5C5C'
+  },
+});
