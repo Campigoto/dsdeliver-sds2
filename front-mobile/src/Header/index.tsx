d36962ada0cbf58c95fd2917e608efcec00f5648
campigoto <campigoto@hotmail.com>
2021-01-11 23:20:01 -0300

    Add Home page

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>DS Delivery</Text>
+        
+    <Image source={require('../assets/logo.png')} />
+      <Text style={styles.text}>DS Delivery</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#DA5C5C'
+    backgroundColor: '#DA5C5C',
+    height: 90,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center'
   },
+  text:{
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.024,
+    color: '#FFF',
+    marginLeft:15,
+    fontFamily: 'OpenSans_700Bold'
+  }
 });
